5ace8b7e25aaa824f81699f1966ef2ee99eb1991
tlcMattLarson <30934703+tlcMattLarson@users.noreply.github.com>
2017-08-11 13:27:10 -0500

    Some Updates and Fixes
    Title says it all.

--- ContactApp/Models/ContactModel.Designer.vb
+++ ContactApp/Models/ContactModel.Designer.vb
@@ -1,4 +1,4 @@
-﻿' T4 code generation is enabled for model 'C:\Users\matthewb\Documents\Visual Studio 2015\Projects\ContactApp\ContactApp\Models\ContactContext.edmx'. 
+﻿' T4 code generation is enabled for model 'C:\Users\matthewb\Documents\Visual Studio 2015\Projects\ContactApp\tlcContactApp\ContactApp\Models\ContactModel.edmx'. 
 ' To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 ' property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 ' is open in the designer.
